a121fcaf8d91db407726ae437c7510f8357c4bd9
EC2 Default User <ec2-user@ip-10-1-9-78.ap-northeast-1.compute.internal>
2022-07-06 04:47:36 +0000

    #4done

--- app.rb
+++ app.rb
@@ -18,11 +18,21 @@ end
 post '/new' do
     Contribution.create({
         name: params[:user_name],
-        body: params[:body]
+        body: params[:body],
+        good: 0
     })
     redirect '/'
 end
 
+post '/good/:id' do
+    content = Contribution.find(params[:id])
+    good = content.good
+    content.update({
+        good: good + 1
+    })
+    redirect '/'
+end
+    
 post '/delete/:id' do
     Contribution.find(params[:id]).destroy
     redirect '/'
